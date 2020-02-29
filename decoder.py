c02335ecd50b8543e628fff69aec6f91d49062cd
abozan <anil.bozan@gmail.com>
2020-02-29 16:20:41 -0500

    callsign and NL functions

--- decoder.py
+++ decoder.py
@@ -1,20 +1,80 @@
 # ABS-B Message Decoder
+import numpy as np
 
 def hex2bin(hexstr):
     """Convert a hexidecimal string to binary string"""
-    binstr = bin(int(hexstr, 16))[2:]
+    scale = 16 ## equals to hexdecimal
+    num_of_bits = 8
+    binstr = bin(int(hexstr, scale))[2:].zfill(num_of_bits)
     return binstr
 
 def bin2int(binstr):
-    """Convert a binary string to integer"""
+    """Convert a binary string to decimal"""
     return int(binstr, 2)
 
 def df(msg):
     """Decode downlink format value, bits 1 to 5"""
-    dfbin = hex2bin(msg[:2])
-    return bin2int(dfbin[:5])
+    msgbin = hex2bin(msg)
+    return bin2int(msgbin[:5])
 
 def ca(msg):
     """Decode capability identifier, bits 6 to 8"""
-    cabin = hex2bin(msg[:2])
-    return bin2int(cabin[5:8])
+    msgbin = hex2bin(msg)
+    return bin2int(msgbin[5:8])
+
+def icao(msg):
+    """Decode ICAO address, bits 9 to 32"""
+    msgbin = hex2bin(msg)
+    return msgbin[8:32]
+
+def tc(msg):
+    """Decode typecode, bits 33 to 37"""
+    msgbin = hex2bin(msg)
+    return bin2int(msgbin[32:37])
+
+def ec(msg):
+    """Decode emitter category, bits 38 to 40"""
+    msgbin = hex2bin(msg)
+    return bin2int(msgbin[37:40])
+
+def callsign(msg):
+    """Decode callsign, bits 41 to 88"""
+    chars = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ#####_###############0123456789######"
+    msgbin = hex2bin(msg)
+    
+    cs = ""
+    cs += chars[bin2int(msgbin[40:46])]
+    cs += chars[bin2int(msgbin[46:52])]
+    cs += chars[bin2int(msgbin[52:58])]
+    cs += chars[bin2int(msgbin[58:64])]
+    cs += chars[bin2int(msgbin[64:70])]
+    cs += chars[bin2int(msgbin[70:76])]
+    cs += chars[bin2int(msgbin[76:82])]
+    cs += chars[bin2int(msgbin[82:88])]
+
+    if (cs.find('_') != -1):
+        cs = cs.replace('_','')
+
+    if (cs.find('#') != -1):
+        cs = cs.replace('#','')
+
+    return cs
+
+def cprNL(lat):
+    """NL() function in CPR decoding."""
+    
+    if lat == 0:
+        return 59
+
+    if lat == 87 or lat == -87:
+        return 2
+
+    if lat > 87 or lat < -87:
+        return 1
+
+    nz = 15
+    a = 1 - np.cos(np.pi / (2 * nz))
+    b = np.cos(np.pi / 180.0 * abs(lat)) ** 2
+    nl = 2 * np.pi / (np.arccos(1 - a / b))
+    NL = np.floor(nl)
+    return NL
--- test_module.py
+++ test_module.py
@@ -1,5 +1,6 @@
 import unittest
-from decoder import df, ca
+from decoder import df, ca, icao, tc, ec, callsign, cprNL
+
 
 class TestDecoder(unittest.TestCase):
     msg = "8D4840D6202CC371C32CE0576098"
@@ -9,6 +10,24 @@ class TestDecoder(unittest.TestCase):
 
     def test_ca(self):
         self.assertEqual(ca(TestDecoder.msg), 5)
+
+    def test_icao(self):
+        self.assertEqual(icao(TestDecoder.msg), "010010000100000011010110")
+
+    def test_tc(self):
+        self.assertEqual(tc(TestDecoder.msg), 4)
+
+    def test_ec(self):
+        self.assertEqual(ec(TestDecoder.msg), 0)
+
+    def test_callsign(self):
+        self.assertEqual(callsign(TestDecoder.msg), "KLM1023")
+
+    def test_cprNL(self):
+        self.assertEqual(cprNL(87), 2)
+        self.assertEqual(cprNL(0), 59)
+        self.assertEqual(cprNL(-88), 1)
+        self.assertEqual(cprNL(30), 51)
 
 if __name__ == '__main__':
     unittest.main()
