cdff7762c91f1352ad1aa1bbb36cf108f1471aac
abozan <anil.bozan@gmail.com>
2020-03-02 21:25:55 -0500

    airborne position

--- decoder.py
+++ decoder.py
@@ -27,7 +27,7 @@ def icao(msg):
     msgbin = hex2bin(msg)
     return msgbin[8:32]
 
-def tc(msg):
+def typecode(msg):
     """Decode typecode, bits 33 to 37"""
     msgbin = hex2bin(msg)
     return bin2int(msgbin[32:37])
@@ -78,3 +78,116 @@ def cprNL(lat):
     nl = 2 * np.pi / (np.arccos(1 - a / b))
     NL = np.floor(nl)
     return NL
+
+def oeFlag(msg):
+    """Check the odd/even flag. Bit 54, 0 for even, 1 for odd.
+    Args:
+        msg (string): 28 bytes hexadecimal message string
+    Returns:
+        int: 0 or 1
+    """
+    if typecode(msg) < 5 or typecode(msg) > 18:
+        raise RuntimeError("%s: Not a position message" % msg)
+
+    msgbin = hex2bin(msg)
+    return int(msgbin[53])
+
+def cprlat(msg):
+    """CPR encoded latitude
+    Args: 
+        msg (string): 28 bytes hexadecimal message string
+    Returns:
+        int: encoded latitude (in 17 bits, 2^17 = 131072)
+    """
+    if typecode(msg) < 5 or typecode(msg) > 18:
+        raise RuntimeError("%s: Not a position message" % msg)
+
+    msgbin = hex2bin(msg)
+    return bin2int(msgbin[54:71])
+
+def cprlon(msg):
+    """CPR encoded longitude
+    Args:
+        msg (string): 28 bytes hexadecimal message string
+    Returns:
+        int: encoded longitude
+    """
+    if typecode(msg) < 5 or typecode(msg) > 18:
+        raise RuntimeError("%s: Not a position message" % msg)
+
+    msgbin = hex2bin(msg)
+    return bin2int(msgbin[71:88])
+
+def airborne_position(msg0, msg1, t0, t1):
+    """Decode airborne position from pair of even and odd position message
+        131072 is 2^17, since CPR lat and lon are 17 bits each
+    Args:
+        msg0 (string): even message (28 bytes hexadecimal string)
+        msg1 (string): odd message (28 bytes hexadecimal string)
+        t0 (int): timestamps for the even message
+        t1 (int): timestamps for the odd message
+    Returns:
+        (float, float): (latitude, longitude) of aircraft
+    """
+    cprlat_even = cprlat(msg0) / 131072.0
+    cprlon_even = cprlon(msg0) / 131072.0
+    cprlat_odd  = cprlat(msg1) / 131072.0
+    cprlon_odd  = cprlon(msg1) / 131072.0
+
+    # calculate the latitude index j
+    j = np.floor(59 * cprlat_even - 60 * cprlat_odd + 0.5)
+
+    d_lat_even = 360 / 60
+    d_lat_odd  = 360 / 59
+
+    lat_even = d_lat_even * (j % 60 + cprlat_even)
+    lat_odd  = d_lat_odd * (j % 59 + cprlat_odd)
+
+    if lat_even >= 270:
+        lat_even -= 360
+    
+    if lat_odd >= 270:
+        lat_od -= 360
+
+    # check if both are in the same latitude zone; exit if not
+    if cprNL(lat_even) != cprNL(lat_odd):
+        return Non
+
+    # compute ni, longitude index m, and longitude
+    if t0 > t1:
+        ni = max(cprNL(lat_even), 1)
+        m = np.floor(cprlon_even * (cprNL(lat_even) - 1) - 
+                     cprlon_odd * cprNL(lat_even) + 0.5)
+        lon = (360.0 / ni) * (m % ni + cprlon_even)
+        lat = lat_even
+    else:
+        ni = max(cprNL(lat_odd), 1)
+        m = np.floor(cprlon_even * (cprNL(lat_odd) - 1) -
+                     cprlon_odd * cprNL(lat_odd) + 0.5)
+        lon = (360.0 / ni) * (m % ni + cprlon_odd)
+        lat = lat_odd
+
+    if lon > 180:
+        lon -= 360
+
+    return round(lat, 5), round(lon, 5)
+
+def altitude(msg):
+    """Decode aircraft altitude
+    Args:
+        msg (string): 28 bytes hexadecimal message string
+    Returns:
+        int: altitude in feet
+    """
+    if typecode(msg) < 9 or typecode(msg) > 18:
+        raise RuntimeError("%s: Not a position message" % msg)
+
+    msgbin = hex2bin(msg)
+    q = msgbin[47]
+
+    if q:
+        n = bin2int(msgbin[40:47]+msgbin[48:52])
+        alt = n * 25 - 1000
+        return alt
+    else:
+        return None
--- test_module.py
+++ test_module.py
@@ -1,9 +1,10 @@
 import unittest
-from decoder import df, ca, icao, tc, ec, callsign, cprNL
-
+from decoder import df, ca, icao, typecode, ec, callsign, cprNL, oeFlag, cprlat, cprlon, airborne_position, altitude
 
 class TestDecoder(unittest.TestCase):
     msg = "8D4840D6202CC371C32CE0576098"
+    msg_pos1 = "8D40621D58C382D690C8AC2863A7"
+    msg_pos2 = "8D40621D58C386435CC412692AD6"
 
     def test_df(self):
         self.assertEqual(df(TestDecoder.msg), 17)
@@ -14,8 +15,8 @@ class TestDecoder(unittest.TestCase):
     def test_icao(self):
         self.assertEqual(icao(TestDecoder.msg), "010010000100000011010110")
 
-    def test_tc(self):
-        self.assertEqual(tc(TestDecoder.msg), 4)
+    def test_typecode(self):
+        self.assertEqual(typecode(TestDecoder.msg), 4)
 
     def test_ec(self):
         self.assertEqual(ec(TestDecoder.msg), 0)
@@ -28,6 +29,24 @@ class TestDecoder(unittest.TestCase):
         self.assertEqual(cprNL(0), 59)
         self.assertEqual(cprNL(-88), 1)
         self.assertEqual(cprNL(30), 51)
+
+    def test_oeFlag(self):
+        self.assertEqual(oeFlag(TestDecoder.msg_pos1), 0)
+        self.assertEqual(oeFlag(TestDecoder.msg_pos2), 1)
+
+    def test_cprlat(self):
+        self.assertEqual(cprlat(TestDecoder.msg_pos1), 93000)
+        self.assertEqual(cprlat(TestDecoder.msg_pos2), 74158)
+
+    def test_cprlon(self):
+        self.assertEqual(cprlon(TestDecoder.msg_pos1), 51372)
+        self.assertEqual(cprlon(TestDecoder.msg_pos2), 50194)
+
+    def test_airborne_position(self):
+        self.assertEqual(airborne_position(TestDecoder.msg_pos1, TestDecoder.msg_pos2,1,0), (52.25720, 3.91937))
+
+    def test_altitude(self):
+        self.assertEqual(altitude(TestDecoder.msg_pos1), 38000)
 
 if __name__ == '__main__':
     unittest.main()
